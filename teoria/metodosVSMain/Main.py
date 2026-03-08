8775bb41fc51f9cc31862efe5b02b7813ca46605
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-07 22:41:24 -0600

    Update Main.py

--- teoria/metodosVSMain/Main.py
+++ teoria/metodosVSMain/Main.py
@@ -1,1 +1,15 @@
+from Cuenta import *
+from Menu import *
 
+
+class Main:
+    pass
+
+
+# Ejecución
+menu = Menu("Bienvenido al Banco Rockerin\nEliga la opción que desee:")
+cuenta = Cuenta("02/03/2006", "Débito", 95000)
+
+menu.darBienvenida()
+menu.despliegaMenu()
+menu.procesaOpcion(cuenta)
