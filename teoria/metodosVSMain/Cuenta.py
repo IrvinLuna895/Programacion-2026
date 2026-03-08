3458cdc6d216a33f696ea3a777500ec057f4c22e
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-07 22:41:00 -0600

    Update Cuenta.py

--- teoria/metodosVSMain/Cuenta.py
+++ teoria/metodosVSMain/Cuenta.py
@@ -1,38 +1,28 @@
-class Menu:
-    def __init__(self, mensaje, fecha, tipo, saldo):
-        self.mensajeBienvenida = mensaje
-        self.fechaCreacion = fecha
-        self.tipoCuenta = tipo
+''' 
+Created on Marzo,2026 
+@author: irvinluna
+
+'''
+
+class Cuenta:
+    def __init__(self, fechaCreacion, tipoCuenta, saldo):
+        self.fechaCreacion = fechaCreacion
+        self.tipoCuenta = tipoCuenta
         self.saldo = saldo
 
-    def darBienvenida(self):
-        print(self.mensajeBienvenida)
+    def depositar(self, cantidad):
+        self.saldo += cantidad
+        print("Depósito exitoso. Nuevo saldo:", self.saldo)
 
-    def despliegaOpciones(self):
-        print("1. Depositar")
-        print("2. Retirar")
-        print("3. Ver información de la cuenta")
-
-        opcion = input("Seleccione una opción (1, 2 o 3): ")
-
-        if opcion == "1":
-            cantidad = float(input("Ingrese la cantidad a depositar: "))
-            self.saldo += cantidad
-            print("Depósito exitoso. Nuevo saldo:", self.saldo)
-
-        elif opcion == "2":
-            cantidad = float(input("Ingrese la cantidad a retirar: "))
-            if cantidad <= self.saldo:
-                self.saldo -= cantidad
-                print("Retiro exitoso. Nuevo saldo:", self.saldo)
-            else:
-                print("Fondos insuficientes.")
-
-        elif opcion == "3":
-            print("--- Información de la cuenta ---")
-            print("Fecha de creación:", self.fechaCreacion)
-            print("Tipo de cuenta:", self.tipoCuenta)
-            print("Saldo actual:", self.saldo)
-
+    def retirar(self, cantidad):
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+            print("Retiro exitoso. Nuevo saldo:", self.saldo)
         else:
-            print("Opción no válida.")
+            print("Fondos insuficientes.")
+
+    def mostrarInformacion(self):
+        print("\n--- Información de la cuenta ---")
+        print("Fecha de creación:", self.fechaCreacion)
+        print("Tipo de cuenta:", self.tipoCuenta)
+        print("Saldo actual:", self.saldo)
