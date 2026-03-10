641022fa6455f999efe495f525fd92edf960f017
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-09 21:31:59 -0600

    Update Cuenta.py

--- teoria/operadorPunto/Cuenta.py
+++ teoria/operadorPunto/Cuenta.py
@@ -1,1 +1,18 @@
+#Cuenta.py
 
+class Cuenta :
+  def __init__(self,valor,taip,fecha):
+    self.saldo = valor
+    self.tipo = taip
+    self.fechaCreacion = fecha
+
+  def depositar(self,cantidad):
+    self.saldo = self.saldo + cantidad
+
+  def retirar(self,cantidad):
+    self.saldo= self.saldo - cantidad
+
+  def informacion(self):
+    print("El saldo de tu cuenta es:", self.saldo)
+    print("El tipo de cuenta es:", self.tipo)
+    print("La fecha de creacion de la cuenta es:", self.fechaCreacion)
