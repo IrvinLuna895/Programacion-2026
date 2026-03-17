1554a0680150b8a70a18bf074ab6b291c836a038
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:45:25 -0600

    Update Cliente.py

--- teoria/metodos Vs Main STR/Cliente.py
+++ teoria/metodos Vs Main STR/Cliente.py
@@ -1,1 +1,20 @@
+''' 
+Created on March,2026
+@author: irvinluna
 
+''' 
+
+from Cuenta import *
+
+class Cliente:
+    def __init__(self, nombre, direccion, edad, cuenta):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.edad = edad
+        self.cuenta = cuenta  
+
+    def __str__(self):
+        return (f"Nombre: {self.nombre}\n"
+                f"Dirección: {self.direccion}\n"
+                f"Edad: {self.edad}\n"
+                f"{self.cuenta}") 
