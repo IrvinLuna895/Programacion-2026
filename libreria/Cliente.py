fd6cc8d3efcf20c582cee37c65493d732a6ac6ef
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:52:45 -0600

    Update Cliente.py

--- libreria/Cliente.py
+++ libreria/Cliente.py
@@ -1,1 +1,10 @@
+class Cliente:
+    def __init__(self, nombre, edad, biblioteca):
+        self.nombre = nombre
+        self.edad = edad
+        self.biblioteca = biblioteca
 
+    def __str__(self):
+        return (f"Cliente: {self.nombre}\n"
+                f"Edad: {self.edad}\n"
+                f"{self.biblioteca}")  # llama al str de Biblioteca
