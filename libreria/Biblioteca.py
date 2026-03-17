d95ab508a1d4cee117a4a064f230f3e00d7c61bc
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:51:50 -0600

    Update Biblioteca.py

--- libreria/Biblioteca.py
+++ libreria/Biblioteca.py
@@ -1,14 +1,18 @@
 #Biblioteca.py
+''' 
+Created on March,2026
+@author: irvinluna
 
-class Biblioteca :
-  def __init__(self,libros):
+''' 
+class Biblioteca:
+    def __init__(self, libros):
         self.libros_disponibles = libros
 
-  def prestar(self,cantidad):
-    self.libros_disponibles = self.libros_disponibles - cantidad
-    print("Préstamo realizado")
+    def prestar(self, cantidad):
+        self.libros_disponibles -= cantidad
 
+    def devolver(self, cantidad):
+        self.libros_disponibles += cantidad
 
-  def devolver(self,cantidad):
-    self.libros_disponibles = self.libros_disponibles + cantidad
-    print("Libros devueltos correctamente")
+    def __str__(self):
+        return f"Libros disponibles: {self.libros_disponibles}"
