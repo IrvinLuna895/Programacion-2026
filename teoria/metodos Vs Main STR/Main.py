64c83a95ed674e36c889fd51a9fc98c4c4241707
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:48:02 -0600

    Update Main.py

--- teoria/metodos Vs Main STR/Main.py
+++ teoria/metodos Vs Main STR/Main.py
@@ -1,1 +1,12 @@
+from Cuenta import *
+from Cliente import *
+from Menu import *
 
+class Main:
+  pass
+cuenta = Cuenta("02/03/2006", "Débito", 95000)
+cliente = Cliente("Irvin Luna", "CDMX", 19, cuenta)
+
+menu = Menu("Bienvenido al Banco Rockerín")
+menu.mostrar()
+menu.ejecutar(cliente)
