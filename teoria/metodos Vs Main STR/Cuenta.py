30607be5f45beb97c1102b6e1ddd7abca482d382
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:45:59 -0600

    Update Cuenta.py

--- teoria/metodos Vs Main STR/Cuenta.py
+++ teoria/metodos Vs Main STR/Cuenta.py
@@ -1,1 +1,14 @@
-$temp
+class Cuenta:
+    def __init__(self, fechaCreacion, tipoCuenta, saldo):
+        self.fechaCreacion = fechaCreacion
+        self.tipoCuenta = tipoCuenta
+        self.saldo = saldo
+
+    def depositar(self, cantidad):
+        self.saldo += cantidad
+
+    def retirar(self, cantidad):
+        self.saldo -= cantidad
+
+    def __str__(self):
+        return f"Saldo: {self.saldo} | Tipo de cuenta: {self.tipoCuenta} | Fecha: {self.fechaCreacion}"
