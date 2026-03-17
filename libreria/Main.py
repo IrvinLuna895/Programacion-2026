230375a942b3813b53d5feb6de48366f7b7e53e1
IrvinLuna895 <irvinluna895@gmail.com>
2026-03-16 22:54:20 -0600

    Update Main.py

--- libreria/Main.py
+++ libreria/Main.py
@@ -1,47 +1,18 @@
-class Menu:
-    def __init__(self, mensaje):
-        self.mensaje = mensaje
+from biblioteca import Biblioteca
+from cliente import Cliente
+from menu import Menu
 
-    def mostrar(self):
-        print(self.mensaje)
-        print("1. Prestar libros")
-        print("2. Devolver libros")
-        print("3. Ver cliente y biblioteca")
-        print("4. Salir")
 
-    def ejecutar(self, cliente):
-        intentos = 0
+class Main:
+  pass
 
-        while intentos < 3:
-            opcion = input("Selecciona una opción: ")
+def main():
+    biblioteca = Biblioteca(50)
+    cliente = Cliente("Irvin", 20, biblioteca)
 
-            if opcion == "1":
-                cantidad = int(input("¿Cuántos libros quieres prestar?: "))
-                if cantidad > 0 and cantidad <= cliente.biblioteca.libros_disponibles:
-                    cliente.biblioteca.prestar(cantidad)
-                    print("Préstamo realizado")
-                else:
-                    print("Cantidad inválida o no hay suficientes libros")
+    menu = Menu(" Bienvenido al sistema de Biblioteca")
+    menu.mostrar()
+    menu.ejecutar(cliente)
 
-            elif opcion == "2":
-                cantidad = int(input("¿Cuántos libros quieres devolver?: "))
-                if cantidad > 0:
-                    cliente.biblioteca.devolver(cantidad)
-                    print("Devolución realizada")
-                else:
-                    print("Cantidad inválida")
-
-            elif opcion == "3":
-                print("\n--- Información del cliente ---")
-                print(cliente)
-
-            elif opcion == "4":
-                print("Saliendo...")
-                break
-
-            else:
-                intentos += 1
-                print(f"Opción inválida ({intentos}/3)")
-
-        if intentos == 3:
-            print("Sistema bloqueado")
+if __name__ == "__main__":
+    main()
