21c76db378860b4e4d948782a41710bfc07c3e84
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:23:08 -0600

    Update Cliente.py

--- teoria/relacionesEntreObjetos/Cliente.py
+++ teoria/relacionesEntreObjetos/Cliente.py
@@ -1,1 +1,18 @@
-asd
+from Cuenta import *
+
+class Cliente:
+    def __init__(self, nombre, cuenta):
+        self.nombre = nombre
+        self.cuenta = cuenta  # ← el cliente tiene una cuenta
+
+    def depositar(self, cantidad):
+        print(f"\nCliente: {self.nombre}")
+        self.cuenta.depositar(cantidad)
+
+    def retirar(self, cantidad):
+        print(f"\nCliente: {self.nombre}")
+        self.cuenta.retirar(cantidad)
+
+    def mostrarInformacion(self):
+        print(f"\n--- Cliente: {self.nombre} ---")
+        self.cuenta.mostrarInformacion()
