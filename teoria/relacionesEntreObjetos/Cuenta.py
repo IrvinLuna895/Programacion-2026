cc8209ed3a12b2103cabf32855d08348b5ca49aa
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:23:52 -0600

    Update Cuenta.py

--- teoria/relacionesEntreObjetos/Cuenta.py
+++ teoria/relacionesEntreObjetos/Cuenta.py
@@ -1,1 +1,37 @@
+''' 
+Created on Marzo,2026 
+@author: irvinluna
 
+'''
+
+
+class Cuenta:
+    def __init__(self, fechaCreacion, tipoCuenta, saldo):
+        self.fechaCreacion = fechaCreacion
+        self.tipoCuenta = tipoCuenta
+        self.saldo = saldo
+
+    def depositar(self, cantidad):
+        if cantidad <= 0:
+            print("Error: la cantidad debe ser mayor que 0.")
+            return
+
+        self.saldo += cantidad
+        print("Depósito exitoso. Nuevo saldo:", self.saldo)
+
+    def retirar(self, cantidad):
+        if cantidad <= 0:
+            print("Error: la cantidad debe ser mayor que 0.")
+            return
+
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+            print("Retiro exitoso. Nuevo saldo:", self.saldo)
+        else:
+            print("Fondos insuficientes.")
+
+    def mostrarInformacion(self):
+        print("\n--- Información de la cuenta ---")
+        print("Fecha de creación:", self.fechaCreacion)
+        print("Tipo de cuenta:", self.tipoCuenta)
+        print("Saldo actual:", self.saldo)
