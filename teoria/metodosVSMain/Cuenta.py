ab8edc33f8339434c4328f05e59bd0069e261574
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:11:49 -0600

    Update Cuenta.py

--- teoria/metodosVSMain/Cuenta.py
+++ teoria/metodosVSMain/Cuenta.py
@@ -11,10 +11,18 @@ class Cuenta:
         self.saldo = saldo
 
     def depositar(self, cantidad):
+        if cantidad <= 0:
+            print("Error: la cantidad debe ser mayor que 0.")
+            return
+
         self.saldo += cantidad
         print("Depósito exitoso. Nuevo saldo:", self.saldo)
 
     def retirar(self, cantidad):
+        if cantidad <= 0:
+            print("Error: la cantidad debe ser mayor que 0.")
+            return
+
         if cantidad <= self.saldo:
             self.saldo -= cantidad
             print("Retiro exitoso. Nuevo saldo:", self.saldo)
