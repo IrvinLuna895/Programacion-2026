2911e7e8187ab053a2795308fb2a4e04c3565072
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:41:02 -0600

    Update Cuenta.py

--- teoria/condicionalesPD/Cuenta.py
+++ teoria/condicionalesPD/Cuenta.py
@@ -1,1 +1,32 @@
 
+
+
+class Cuenta:
+    def __init__(self, fechaCreacion, tipoCuenta, saldo):
+        self.fechaCreacion = fechaCreacion
+        self.tipoCuenta = tipoCuenta
+        self.saldo = saldo
+
+    def __str__(self):
+        return (
+            "\n--- Información de la cuenta ---\n"
+            f"Fecha de creación: {self.fechaCreacion}\n"
+            f"Tipo de cuenta: {self.tipoCuenta}\n"
+            f"Saldo actual: {self.saldo}"
+        )
+
+    def depositar(self, cantidad):
+        if cantidad <= 0:
+            return False
+        self.saldo += cantidad
+        return True
+
+    def retirar(self, cantidad):
+        if cantidad <= 0:
+            return False
+
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+            return True
+
+        return False
