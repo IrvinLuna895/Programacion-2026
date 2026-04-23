382f2577a5ce6ae22f37640de55e6636635f819c
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:25:46 -0600

    Update Main.py

--- teoria/relacionesEntreObjetos/Main.py
+++ teoria/relacionesEntreObjetos/Main.py
@@ -1,1 +1,20 @@
+from Cuenta import *
+from Cliente import *
+from Menu import *
 
+
+class Main:
+    pass
+# Crear cuenta
+cuenta = Cuenta("02/03/2006", "Débito", 95000)
+
+# Crear cliente con su cuenta
+cliente = Cliente("Irvin", cuenta)
+
+# Crear menú
+menu = Menu("Bienvenido al Banco Rockerin\nElija la opción que desee:")
+
+# Ejecutar sistema
+menu.darBienvenida()
+menu.despliegaMenu()
+menu.procesaOpcion(cliente)
