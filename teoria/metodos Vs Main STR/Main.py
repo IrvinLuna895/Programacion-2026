b21750426d191b39c3f2d8992a275f98295bb58a
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 00:36:04 -0600

    Update Main.py

--- teoria/metodos Vs Main STR/Main.py
+++ teoria/metodos Vs Main STR/Main.py
@@ -4,9 +4,12 @@ from Menu import *
 
 class Main:
   pass
-cuenta = Cuenta("02/03/2006", "Débito", 95000)
-cliente = Cliente("Irvin Luna", "CDMX", 19, cuenta)
 
-menu = Menu("Bienvenido al Banco Rockerín")
-menu.mostrar()
-menu.ejecutar(cliente)
+cuenta = Cuenta("02/03/2006", "Débito", 95000)
+cliente = Cliente("Irvin", cuenta)
+
+menu = Menu("Bienvenido al Banco Rockerin\nElija la opción que desee:")
+
+menu.darBienvenida()
+menu.despliegaMenu()
+menu.procesaOpcion(cliente)
