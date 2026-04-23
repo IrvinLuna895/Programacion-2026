fd993236f3601c81f854130b804f2ba82571ac93
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-23 01:08:23 -0600

    Update Cuenta.py

--- teoria/listasDeCuentas/Cuenta.py
+++ teoria/listasDeCuentas/Cuenta.py
@@ -1,1 +1,41 @@
+''' 
+Created on Abril,2026 
+@author: irvinluna
 
+'''
+
+class Cuenta:
+    def __init__(self, fechaCreacion, tipoCuenta, saldo):
+        self.__fechaCreacion = fechaCreacion
+        self.__tipoCuenta = tipoCuenta
+        self.__saldo = saldo
+
+    def get_fechaCreacion(self):
+        return self.__fechaCreacion
+
+    def get_tipoCuenta(self):
+        return self.__tipoCuenta
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def depositar(self, cantidad):
+        if cantidad <= 0:
+            return False
+        self.__saldo += cantidad
+        return True
+
+    def retirar(self, cantidad):
+        if cantidad <= 0:
+            return False
+        if cantidad <= self.__saldo:
+            self.__saldo -= cantidad
+            return True
+        return False
+
+    def __str__(self):
+        return (
+            f"Tipo: {self.get_tipoCuenta()} | "
+            f"Saldo: {self.get_saldo()} | "
+            f"Fecha: {self.get_fechaCreacion()}"
+        )
