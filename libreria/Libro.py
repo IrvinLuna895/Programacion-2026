023e2bc38a1adb97c677b8cb4714097b9114f869
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-26 22:58:17 -0600

    Update Libro.py

--- libreria/Libro.py
+++ libreria/Libro.py
@@ -1,1 +1,36 @@
 
+
+
+
+class Libro:
+    def __init__(self, titulo, autor, disponible=True):
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__disponible = disponible
+
+    # GETTERS
+    def get_titulo(self):
+        return self.__titulo
+
+    def get_autor(self):
+        return self.__autor
+
+    def get_disponible(self):
+        return self.__disponible
+
+    # MÉTODOS
+    def prestar(self):
+        if self.__disponible:
+            self.__disponible = False
+            return True
+        return False
+
+    def devolver(self):
+        if not self.__disponible:
+            self.__disponible = True
+            return True
+        return False
+
+    def __str__(self):
+        estado = "Disponible" if self.get_disponible() else "Prestado"
+        return f"{self.get_titulo()} - {self.get_autor()} ({estado})"
