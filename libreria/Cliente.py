35ba6817b4d21009f5fda5a2143be71a382e82a8
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-26 22:59:47 -0600

    Update Cliente.py

--- libreria/Cliente.py
+++ libreria/Cliente.py
@@ -1,14 +1,36 @@
 
-from Biblioteca import
-
-
 class Cliente:
-    def __init__(self, nombre, edad, biblioteca):
-        self.nombre = nombre
-        self.edad = edad
-        self.biblioteca = biblioteca
+    def __init__(self, nombre, edad):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__librosPrestados = []
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_edad(self):
+        return self.__edad
+
+    def get_librosPrestados(self):
+        return self.__librosPrestados
+
+    def agregarPrestamo(self, libro):
+        self.__librosPrestados.append(libro)
+
+    def devolverPrestamo(self, libro):
+        if libro in self.__librosPrestados:
+            self.__librosPrestados.remove(libro)
+            return True
+        return False
 
     def __str__(self):
-        return (f"Cliente: {self.nombre}\n"
-                f"Edad: {self.edad}\n"
-                f"{self.biblioteca}")  # llama al str de Biblioteca
+        info = f"\nCliente: {self.get_nombre()} | Edad: {self.get_edad()}\n"
+        info += "Libros prestados:\n"
+
+        if not self.__librosPrestados:
+            info += "Ninguno\n"
+        else:
+            for libro in self.__librosPrestados:
+                info += f"- {libro.get_titulo()}\n"
+
+        return info
