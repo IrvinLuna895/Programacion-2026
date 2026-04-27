9ae47d3b61254bdfcc06d6af6e70a611e0cf9dac
IrvinLuna895 <irvinluna895@gmail.com>
2026-04-26 22:59:01 -0600

    Update Biblioteca.py

--- libreria/Biblioteca.py
+++ libreria/Biblioteca.py
@@ -1,18 +1,45 @@
-#Biblioteca.py
-''' 
-Created on March,2026
-@author: irvinluna
 
-''' 
+from Libro import *
+
 class Biblioteca:
-    def __init__(self, libros):
-        self.libros_disponibles = libros
+    def __init__(self, nombre):
+        self.__nombre = nombre
+        self.__libros = []
 
-    def prestar(self, cantidad):
-        self.libros_disponibles -= cantidad
+    def get_nombre(self):
+        return self.__nombre
 
-    def devolver(self, cantidad):
-        self.libros_disponibles += cantidad
+    def get_libros(self):
+        return self.__libros
+
+    def agregarLibro(self, libro):
+        self.__libros.append(libro)
+
+    def eliminarLibro(self, indice):
+        if 0 <= indice < len(self.__libros):
+            self.__libros.pop(indice)
+            return True
+        return False
+
+    def recuperarLibro(self, indice):
+        if 0 <= indice < len(self.__libros):
+            return self.__libros[indice]
+        return None
+
+    def prestarLibro(self, indice):
+        libro = self.recuperarLibro(indice)
+        if libro:
+            return libro.prestar()
+        return False
+
+    def devolverLibro(self, indice):
+        libro = self.recuperarLibro(indice)
+        if libro:
+            return libro.devolver()
+        return False
 
     def __str__(self):
-        return f"Libros disponibles: {self.libros_disponibles}"
+        info = f"\nBiblioteca: {self.get_nombre()}\nCatálogo:\n"
+        for i, libro in enumerate(self.__libros):
+            info += f"[{i}] {libro}\n"
+        return info
